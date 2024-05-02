071f87e7ce23b8b6116a0617e16fce50d3b20135
Kajalnanera <168439666+Kajalnanera@users.noreply.github.com>
2024-05-02 16:00:13 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'custom-theme-name' );
+define( 'DB_NAME', 'custom-theme-name-new' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
